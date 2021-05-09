c71a9960ba6e36a9e80c132de71f9202efbc9d9a
Grayson9352 <67601643+Grayson9352@users.noreply.github.com>
2021-05-09 18:42:13 -0400

    Add files via upload

---
+++ ArgGetterLOGS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string localAD = Environment.GetEnvironmentVariable("LocalAppData");
+            List<string> arguments = new List<string>();
+            string line;
+            using (StreamReader file = new StreamReader(localAD + @"\FortniteGame\Saved\Logs\FortniteGame.log"))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains("fltoken"))
+                    {
+                        arguments.Add(line);
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
