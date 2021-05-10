93799aa902d3318b5be9152f0972de9f46e80d7e
Grayson9352 <67601643+Grayson9352@users.noreply.github.com>
2021-05-10 15:38:20 -0400

    Create ArgGetterBE.java

---
+++ ArgGetterBE.java
@@ -0,0 +1,9 @@
+public class A {  
+  public static void main(String args[]){  
+
+      for(int i=0;i<args.length;i++){
+        System.out.println(args[i]);
+      }
+
+    }  
+  }  // idk if this works eclipse was being dumb
