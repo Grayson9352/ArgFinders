a79a9037b4363a9cb779812ad284dde9ccf2b69d
Grayson9352 <67601643+Grayson9352@users.noreply.github.com>
2021-05-10 14:51:12 -0400

    thank fix xmlscott

--- ArgGetterBE.c
+++ ArgGetterBE.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) { // argc is arg count, argv is the arg variables
-    ofstream outfile("args.txt");
+    FILE *outfile=fopen("args.txt","w");
 
     for (int i = 0; i < argc; ++i) {
-        outfile << argv[i] << "\n";
+        fputs(argv[i], outfile); 
+    fputs("\n", outfile);
+    
     }
 
-    outfile.close();
-} // Thanks to XMLScott for converting the c++ one into C :)
+    fclose(outfile);
+}
+// Thanks to XMLScott for converting the c++ one into C :) and also fixing it 
